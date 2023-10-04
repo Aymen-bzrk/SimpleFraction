5ef25c6c44c9cfe534e735c12d68a98cc90c5e22
unknown <aymen-bzrk@outlook.com>
2023-10-04 11:58:38 +0200

    reponse part 2

--- Fraction.java
+++ Fraction.java
@@ -6,6 +6,14 @@ public class Fraction {
         this.p = p;
         this.q = q;
     }
+    public Fraction(int p) {
+        this.q = 1;
+        this.p = p;
+    }
+    public Fraction() {
+        this.p = 0;
+        this.q = 1;
+    }
     public int getNumerator() {
         return p;
     }
@@ -17,4 +25,19 @@ public class Fraction {
     public String toString() {
         return "Je suis une fraction.";
     }
+
+    public static final Fraction Zero=new fraction();
+    public static final Fraction un=new fraction(1);
+
+     public boolean equals(Object obj) {
+        if (this == obj) {
+        return true;
+    }
+        if (obj == null || getClass() != obj.getClass()) {
+        return false;
+    }
+    Fraction other = (Fraction) obj;
+        assert this.q != 0 && other.q != 0 : "Le dénominateur ne peut pas être zéro.";
+        return this.p == other.p && this.q == other.q;
+}
 }
